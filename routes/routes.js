badee51a7e0c90696497515a4d4c017c0da58240
findLogic <66243299+findLogic@users.noreply.github.com>
2020-06-11 23:10:22 +0300

    update routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,8 @@
 // Load the MySQL pool connection
 const pool = require('../data/config');
+const bodyParser = require('body-parser');
+
+const jsonParser = bodyParser.json();
 
 const router = (app) => {
   // TESTING
@@ -35,32 +38,36 @@ const router = (app) => {
   // USERS SECTION
   ///////////////////
 
-  // Display a single user by ID
-  app.get('/users/:id', (req, res) => {
-    const id = req.params.id;
-    console.log('truing to use id ', id);
-    pool.query('SELECT * FROM users WHERE id = ?', id, (err, result) => {
-      if (err) throw err;
+  // get a single user by login
+  app.get('/users/:str', jsonParser, (req, res) => {
+    const login = req.params.str.split('...')[0].slice(1);
+    const password = req.params.str.split('...')[1];
+    pool.query(
+      'SELECT * FROM users WHERE login = ? and password = ?',
+      [login, password],
 
-      res.send(result);
-    });
+      (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+      },
+    );
   });
 
   // Add a new user
   app.post('/users', (req, res) => {
-    console.log(req.body);
-    pool.query('INSERT INTO users SET ?', req.body, (err, result) => {
+    pool.query('INSERT INTO users SET ?', [req.body], (err, result) => {
       if (err) throw err;
-      res.status(201).send(`User added with ID: ${result.insertId}`);
+      const id = result.insertId;
+      res.status(201).send({ id });
     });
   });
 
   // Update an existing user
   app.put('/users/:id', (req, res) => {
     const id = req.params.id;
-
     pool.query(
-      'UPDATE users SET ? WHERE id = ?',
+      'UPDATE users SET orders=? WHERE id = ?',
       [req.body, id],
       (err, result) => {
         if (err) throw err;
